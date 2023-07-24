7aad08d64dbb0c317dd8fad5f061185858190da6
Amy Smith <smithniamyfse@gmail.com>
2023-07-23 23:28:34 -0500

    updated project to include relevant libraries

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,29 +1,20 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 function MovieDetails() {
   const dispatch = useDispatch();
   const history = useHistory();
-  const movieDetails = useSelector(state => state.movieDetails);
+  const movieDetails = useSelector((state) => state.movieDetails);
   const { id } = useParams(); // get movie ID from route parameters
 
   useEffect(() => {
-    dispatch({ type: 'FETCH_MOVIE_DETAILS', payload: id });
+    dispatch({ type: "FETCH_MOVIE_DETAILS", payload: id });
   }, [dispatch, id]);
 
-
-
-
-return (
-
-
-
-
-
-<button onClick={() => history.push('/')}>Back to List</button>
-);
-
+  return <button onClick={() => history.push("/")}>Back to List</button>;
 }
+
+export default MovieDetails;
